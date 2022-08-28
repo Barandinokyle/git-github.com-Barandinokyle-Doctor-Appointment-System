b7c6234c2658b396f21695c651cd8bbc48365e3b
kyle <barandinokyle01@gmail.com>
2022-08-29 02:36:02 +0800

    first commit

---
+++ PHP-Doctor-Appointment-System/patient/AboutUs.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="About-Us.css" rel="stylesheet">
+</head>
+<body>
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+</body>
+</html>
---
+++ PHP-Doctor-Appointment-System/patient/services.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width-device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet"  href="Services.css">
+    <title>About US</title>
+
+<style>
+.container{
+    position: absolute;
+    bottom: 250px;
+    left: 270px;
+    font-size: 15px;
+    font-family: Verdana;
+    color: white;
+    font-style:initial;
+}
+</style>
+
+
+</head>
+    <body>
+<nav>
+    <div class = "logo">
+        <h4>TELEMEDICINE</h4>
+    </div>
+<ul class="navi" >
+    
+    <li><a href="services.php">About US</a></li>
+    <li><a href="../index.php">Home</a></li>
+    
+</ul>
+</nav>  
+
+
+<div class="container">
+
+<h1>MyHealth Clinic believes that everyone should be able to take care<br>
+&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;of their health and wellness. 
+ As a multi-specialty and<br>&nbsp; &nbsp; &nbsp; &nbsp;  full-service
+ clinic,we empower people to do so by making<br>  the quality services<br>
+they need available with excellent and compassionate patient care.</h1>
+
+
+
+
+</div>
+
+
+
+
+
+    </body>
+</html>
